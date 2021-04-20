a280f0635d93547206ece6fc32ca7f806a138ff8
Gor <7495500@gmail.com>
2021-04-20 16:41:45 +0300

    Update projects and add new helpers

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -9,4 +9,12 @@ module ApplicationHelper
     end.join("\n").html_safe
   end
 
+  def author_of(resource)
+    user_signed_in? && resource.user_id == current_user.id
+  end
+
+  def admin?
+    user_signed_in? && current_user.admin?
+  end
+
 end
