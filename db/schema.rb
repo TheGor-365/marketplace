3bb4392aded6d3ef5a5bb3744fb4c88416e71654
Gor <7495500@gmail.com>
2021-04-20 20:36:16 +0300

    Update User model for stripe

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_20_170208) do
+ActiveRecord::Schema.define(version: 2021_04_20_173403) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -90,6 +90,10 @@ ActiveRecord::Schema.define(version: 2021_04_20_170208) do
     t.boolean "admin", default: false
     t.boolean "maker", default: false
     t.boolean "backer", default: false
+    t.string "uid"
+    t.string "provider"
+    t.string "access_code"
+    t.string "publishable_key"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
     t.index ["username"], name: "index_users_on_username", unique: true
