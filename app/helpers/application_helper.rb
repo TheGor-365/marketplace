0f588220ba44fe43ae89bd00442966222032f513
Gor <7495500@gmail.com>
2021-04-20 23:48:16 +0300

    Add the view layer

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -21,4 +21,10 @@ module ApplicationHelper
     "https://connect.stripe.com/oauth/authorize?response_type=code&client_id=#{Rails.application.credentials.dig(:stripe, :connect_client_id)}&scope=read_write"
   end
 
+  def stripe_connect_button 
+    link_to stripe_url, class: "btn-stripe-connect" do
+      content_tag :span, "Connect with Stripe"
+    end
+  end
+
 end
